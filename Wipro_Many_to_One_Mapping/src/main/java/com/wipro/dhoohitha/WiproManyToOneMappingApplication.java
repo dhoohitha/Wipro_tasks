5708a6bda3dd7c0eedf4f1b78156bf297fca6bae
dhoohitha <116478858+dhoohitha@users.noreply.github.com>
2025-08-19 22:18:57 +0530

    Add files via upload

---
+++ Wipro_Many_to_One_Mapping/src/main/java/com/wipro/dhoohitha/WiproManyToOneMappingApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.dhoohitha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiproManyToOneMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiproManyToOneMappingApplication.class, args);
+	}
+
+}
---
+++ Wipro_Many_to_One_Mapping/src/test/java/com/wipro/dhoohitha/WiproManyToOneMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.dhoohitha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiproManyToOneMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
