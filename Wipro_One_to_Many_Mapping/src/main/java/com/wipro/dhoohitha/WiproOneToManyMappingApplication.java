bd620e5c690512d5d2ad031f67f5483de0e63b10
dhoohitha <116478858+dhoohitha@users.noreply.github.com>
2025-08-19 22:07:37 +0530

    Add files via upload

---
+++ Wipro_One_to_Many_Mapping/src/main/java/com/wipro/dhoohitha/WiproOneToManyMappingApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.dhoohitha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiproOneToManyMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiproOneToManyMappingApplication.class, args);
+	}
+
+}
